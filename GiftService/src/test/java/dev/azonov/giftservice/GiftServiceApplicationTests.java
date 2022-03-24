a13388056a6369fa5c954644adb5381d704c90a7
Andrey Zonov <azonov@foss.dk>
2022-03-24 15:42:14 +0300

    Initial project for gift service

---
+++ GiftService/src/test/java/dev/azonov/giftservice/GiftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.azonov.giftservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
