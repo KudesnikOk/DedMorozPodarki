c89427c3170add37cd1a29f29491e2fc6e25a696
Andrey Zonov <azonov@foss.dk>
2022-03-25 14:33:25 +0300

    Database initialization script

---
+++ GiftService/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO giftkind (name) VALUES ('car')
+INSERT INTO giftkind (name) VALUES ('doll')
+INSERT INTO giftkind (name) VALUES ('book')
+INSERT INTO giftkind (name) VALUES ('phone')
+
+INSERT INTO GIFTS (quantity, kind_id) VALUES (20, 1)
+INSERT INTO GIFTS (quantity, kind_id) VALUES (15, 2)
+INSERT INTO GIFTS (quantity, kind_id) VALUES (30, 3)
+INSERT INTO GIFTS (quantity, kind_id) VALUES (5, 4)
