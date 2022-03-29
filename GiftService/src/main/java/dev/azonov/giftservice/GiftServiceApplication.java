d6dabbe59c7c04b8401e7d1fb7ce4ec0b0d50fa3
Andrey Zonov <azonov@foss.dk>
2022-03-29 13:59:06 +0300

    Add production service as a way to request more gifts to be produced

--- GiftService/src/main/java/dev/azonov/giftservice/GiftServiceApplication.java
+++ GiftService/src/main/java/dev/azonov/giftservice/GiftServiceApplication.java
@@ -2,10 +2,21 @@ package dev.azonov.giftservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GiftServiceApplication {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+        return builder.build();
+    }
+
     public static void main(String[] args) {
+
         SpringApplication.run(GiftServiceApplication.class, args);
     }
 }
