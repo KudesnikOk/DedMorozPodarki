efe1258e3829f8390dc57c6c484027a3b9d46334
Andrey Zonov <azonov@foss.dk>
2022-04-07 14:33:52 +0300

    Remove not used imports

--- GiftService/src/test/java/dev/azonov/giftservice/service/ChildServiceTest.java
+++ GiftService/src/test/java/dev/azonov/giftservice/service/ChildServiceTest.java
@@ -6,11 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
